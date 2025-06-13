bf4c78a94cd120e4c3265e1e43f2cb6f8e18074d
grumpchick <divyacharitha20@gmail.com>
2025-06-13 19:06:27 +0530

    Add project files.

---
+++ C#_step1/1.Basics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+ class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("This is main method ");
+        Console.WriteLine("please provide your name:");
+        string name=Console.ReadLine();
+        Console.WriteLine("Please state your qualifications");
+        string qualifications=Console.ReadLine();
+        Console.WriteLine("What are your intrests");
+        string interest = Console.ReadLine();
+
+
+
+        greeting();
+        study(qualifications);
+        intrestsOfuser(interest,name);
+
+        Console.WriteLine("This is end of main method ");
+        
+        }
+        static void greeting()
+    {
+        Console.WriteLine( "Welcome to CodeWorld! Let’s build something great together.");
+
+    }
+    static void study(string qualifications)
+    {
+        Console.WriteLine("your qualifications are : " + qualifications);
+    }
+    static void intrestsOfuser(string interest,string name)
+    {
+        Console.WriteLine(name + ", it's awesome that you're interested in "+ interest);
+
+    }
+    }
+
