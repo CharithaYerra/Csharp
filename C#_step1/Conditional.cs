337bd16d52e01cfe0c49e8aba0ea0a3e53c8b2f2
grumpchick <divyacharitha20@gmail.com>
2025-06-23 22:20:41 +0530

    conditional

---
+++ C#_step1/Conditional.cs
@@ -0,0 +1,62 @@
+﻿using System;
+ class Conditional
+    {
+    static void Main()
+    {
+        int number = 10;
+
+        if (number > 5)
+        {
+            Console.WriteLine("Number is greater than 5");
+        }
+        int age = 16;
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+        else
+        {
+            Console.WriteLine("You are a minor.");
+        }
+        int marks = 75;
+
+        if (marks >= 90)
+        {
+            Console.WriteLine("Grade A");
+        }
+        else if (marks >= 75)
+        {
+            Console.WriteLine("Grade B");
+        }
+        else if (marks >= 50)
+        {
+            Console.WriteLine("Grade C");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+        int day = 3;
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            default:
+                Console.WriteLine("Invalid day");
+                break;
+        }
+
+
+
+    }
+}
+
