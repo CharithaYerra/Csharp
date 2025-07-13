7c22aa664930892db42165f6fc3378716fd3a28e
grumpchick <divyacharitha20@gmail.com>
2025-07-13 16:10:42 +0530

    task

--- Csharp_step2/C#_step2/2.Properties.cs
+++ Csharp_step2/C#_step2/2.Properties.cs
@@ -1,33 +1,33 @@
-﻿using System;
+﻿//using System;
 
-class StudentResume
-{
-    string filename;
-    string filelocation = @"D:\Students\Resumes";
-    string filesize = "2Kb";
-    string createdDate = "Mar-01-2025";
-    DateTime createdDateTime = DateTime.Now;
+//class StudentResume
+//{
+//    string filename;
+//    string filelocation = @"D:\Students\Resumes";
+//    string filesize = "2Kb";
+//    string createdDate = "Mar-01-2025";
+//    DateTime createdDateTime = DateTime.Now;
 
 
 
-    //Attributes
-    string FileName { get; set; }
-    string FileLocation { get; set; }
-    string FileSize { get; set; }
-    string CreatedDate { get; set; }
+//    //Attributes
+//    string FileName { get; set; }
+//    string FileLocation { get; set; }
+//    string FileSize { get; set; }
+//    string CreatedDate { get; set; }
 
-    static void Main()
-    {
+//    static void Main()
+//    {
 
-        StudentResume studentResume = new StudentResume();
+//        StudentResume studentResume = new StudentResume();
 
-        //studentResume.filename = "Employee1_Salary.pdf";
-        studentResume.FileName = "Employee1_Resume.pdf";
-        studentResume.FileLocation = "C:\\Employees\\Resumes";
-        studentResume.FileSize = "2kb";
-        studentResume.CreatedDate = "Mar-01-2025";
+//        //studentResume.filename = "Employee1_Salary.pdf";
+//        studentResume.FileName = "Employee1_Resume.pdf";
+//        studentResume.FileLocation = "C:\\Employees\\Resumes";
+//        studentResume.FileSize = "2kb";
+//        studentResume.CreatedDate = "Mar-01-2025";
 
-        Console.WriteLine($"FileName is {studentResume.FileName} and fileSize is {studentResume.FileSize}");
+//        Console.WriteLine($"FileName is {studentResume.FileName} and fileSize is {studentResume.FileSize}");
 
-    }
-}
+//    }
+//}
